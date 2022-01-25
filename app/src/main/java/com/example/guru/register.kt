9913df95506c56addfe510efb555792fe119e304
yoonseon <hjwoys@naver.com>
2022-01-25 12:12:51 +0900

    회원가입 화면 구현-1

--- app/src/main/java/com/example/guru/register.kt
+++ app/src/main/java/com/example/guru/register.kt
@@ -1,11 +1,22 @@
 package com.example.guru
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class register : AppCompatActivity() {
+
+    lateinit var btn_register1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_register)
+
+    btn_register1 = findViewById(R.id.btn_register1)
+
+    btn_register1.setOnClickListener {
+        var intent = Intent(this, Login:: class.java)
+        startActivity(intent)
     }
+}
 }
