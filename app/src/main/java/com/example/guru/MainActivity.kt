62990dccb6263e41b09e28bb656815e8d0b482e0
jieun-guru <68229317+jieun-guru@users.noreply.github.com>
2022-01-26 13:20:28 +0900

    25일(최종) 테마 변경 색 선택 dialog 추가

--- app/src/main/java/com/example/guru/MainActivity.kt
+++ app/src/main/java/com/example/guru/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.guru
 
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
+import android.widget.Toast
+import petrov.kristiyan.colorpicker.ColorPicker
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +20,41 @@ class MainActivity : AppCompatActivity() {
         btn_ex = findViewById(R.id.btn_ex)
 
         btn_ex.setOnClickListener {
-            var intent = Intent(this, Login:: class.java)
+            var intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
     }
+
+    // 테마 변경 dialog
+    fun openColorPicker() {
+        val colorPicker = ColorPicker(this)
+        colorPicker.setTitle("변경할 색을 고르세요")
+        val colors = ArrayList<String>()
+
+        colors.add("#9E1A20")
+        colors.add("#101077")
+        colors.add("#FFB300")
+        colors.add("#205723")
+        colors.add("#000000")
+
+        colorPicker.setColors(colors)
+                .setColumns(5)
+                .setRoundColorButton(true)
+                .setOnChooseColorListener(object : ColorPicker.OnChooseColorListener {
+                    override fun onChooseColor(position: Int, color: Int) {
+                        // will be call only when Ok button was tapped
+                        //setTheme(color)
+                        supportActionBar!!.setBackgroundDrawable(ColorDrawable(color))
+
+                        //supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#146775")))
+                        Toast.makeText(applicationContext, "테마 변경이 완료되었습니다", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onCancel() {
+
+                    }
+                }).show()
+    }
+
+
 }
