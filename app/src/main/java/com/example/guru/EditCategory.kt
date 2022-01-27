4db285faae6c790452d08d57783b4e89bd3947b0
jieun-guru <68229317+jieun-guru@users.noreply.github.com>
2022-01-27 10:36:54 +0900

    카테고리 수정 화면 구성

---
+++ app/src/main/java/com/example/guru/EditCategory.kt
@@ -0,0 +1,17 @@
+package com.example.guru
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditCategory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_category)
+
+        // 추가 버튼 클릭 시
+
+        // 수정 버튼 클릭 시
+
+        // 삭제 버튼 클릭 시
+    }
+}
