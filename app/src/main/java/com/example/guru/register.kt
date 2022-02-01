0369cf04589dec7d672449812e01c2d81044a368
yoonseon <hjwoys@naver.com>
2022-02-01 14:17:59 +0900

    시작화면 설정

--- app/src/main/java/com/example/guru/register.kt
+++ app/src/main/java/com/example/guru/register.kt
@@ -10,16 +10,19 @@ class register : AppCompatActivity() {
 
     lateinit var btn_register1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_register)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
 
-    btn_register1 = findViewById(R.id.btn_register1)
+        btn_register1 = findViewById(R.id.btn_register1)
 
-    btn_register1.setOnClickListener {
-        var intent = Intent(this, Login:: class.java)
-        startActivity(intent)
+        btn_register1.setOnClickListener {
+            var intent = Intent(this, Login::class.java)
+            startActivity(intent)
 
+        }
+
+        btn_register1.setOnClickListener {
+            Toast.makeText(this@register, "토스트 메세지 띄우기 입니다.", Toast.LENGTH_SHORT).show()
+        }
     }
-
-}
-}
+    }
